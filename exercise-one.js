60c6c7396e8f85477652794e609e549f3c82717d
minhochoi1983 <59149459+minhochoi1983@users.noreply.github.com>
2020-07-03 21:17:41 -0700

    added exercise-one file

---
+++ exercise-one.js
@@ -0,0 +1,28 @@
+const boeingAircrafts = ['737', '747', '767', '777', '787'];
+
+console.log(boeingAircrafts);
+
+
+
+let boeingAircrafts = ['737', '747', '767', '777', '787'];
+
+boeingAircrafts.push('P8');
+
+console.log(boeingAircrafts);
+
+
+
+let boeingAircrafts = ['737', '747', '767', '777', '787'];
+
+boeingAircrafts.splice(2, 2);
+
+console.log(boeingAircrafts);
+
+
+
+let boeingAircrafts = ['737', '747', '767', '777', '787'];
+
+const boeingAircraftsList = boeingAircrafts.join(',');
+
+console.log(boeingAircrafts);
+console.log(boeingAircraftsList);
